4d72fe19d782300899924da4513fd766971b9ea7
Ian Mabon <ian_mabon@cate.org>
2020-04-27 12:26:23 -0700

    third commit: layout done

--- Cate Sports App/ContentView.swift
+++ Cate Sports App/ContentView.swift
@@ -8,34 +8,88 @@
 
 import SwiftUI
 
+var pickerData: [String] = [String]()
+
 struct ContentView: View {
+    
+    let sportsTeams = ["Baseball", "Boys Lacrosse", "Girls Lacrosse", "Boys Tennis", "Girls Tennis", "Boys Volleyball", "Girls Volleyball", "Swimming", "Track and Field", "Boys Waterpolo", "Girls Waterpolo", "Football", "Cross Country", "Boys Basketball", "Girls Basketball", "Boys Soccer", "Girls Soccer", "Squash", "Ultimate Frisbee"]
+    @State private var selection = ""
+    
     var body: some View {
-        VStack {
+        ZStack {
             VStack {
-        Text("Cate Sports App")
-            .padding(.all)
-            .font(.system(size: 30))
-            .colorInvert()
+                Image("girlz").resizable().edgesIgnoringSafeArea(.all)
+                Image("football").resizable().edgesIgnoringSafeArea(.all)
+                Image("dunk").resizable().edgesIgnoringSafeArea(.all)
+                       }
+        VStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 10).frame(width: 300, height: 50, alignment: .center).colorInvert()
+                    .opacity(0.85)
+                Text("Cate Sports App")
+                    .padding(.all)
+                    .font(.system(size: 30))
+                    
             }
-    Spacer()
-            Rectangle().frame(width: 300, height: 200, alignment: .center).colorInvert()
             Spacer()
-            Rectangle().frame(width: 300, height: 200, alignment: .center).colorInvert()
+            ZStack {
+                
+                Rectangle().frame(width: 320, height: 220, alignment: .center).colorInvert().opacity(0.85)
+                
+                Image("Map").resizable()
+                    .frame(width: 300, height: 200, alignment: .center)
+            }
             Spacer()
-                Text("placeholder")
+        
+                HStack {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 310, height: 125, alignment: .leading)
+                            .foregroundColor(.white).opacity(0.85)
+                    Picker(selection: $selection, label:
+                Text("Sport")) {
+                    
+                    ForEach(0 ..< sportsTeams.count) { index in
+                        
+                        Text(self.sportsTeams[index]).tag(index)
+                    }
+                    
+                }
+                    }
+                    Button(action: {
+                        //notes for specific sport
+                    }) {
+                       Image(systemName: "magnifyingglass.circle.fill")
+                       .resizable()
+                       .foregroundColor(.white)
+                       .frame(width: 50, height: 50, alignment: .center)
+                       .padding(.all)
+                    }
+                    
+                }
+            
             Spacer()
-
+            ZStack {
+                RoundedRectangle(cornerRadius: 10).frame(width: 300, height: 200, alignment: .center).colorInvert().opacity(0.85)
+            Text("Notes for Sport")
+            }
+            Spacer()
+            
         }
-       .background(Image("damn").resizable()
-              .aspectRatio(contentMode: .fill)
-              .edgesIgnoringSafeArea(.all)
-        )
-
+           
+        }
+//        .background(Image("hands").resizable()
+//        .aspectRatio(contentMode: .fill)
+//        .edgesIgnoringSafeArea(.all)
+//        )
+        
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
